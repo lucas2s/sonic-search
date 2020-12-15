278c9caebf7ff1f4d737899a3ebb57eab5ac1697
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2020-12-14 23:22:10 -0300

    Implementação das rotas de cadastro busca e suggest

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,63 @@
 const express = require('express');
+const { v4: uuid } = require('uuid');
+const { Ingest, Search } = require('sonic-channel');
 
 const app = express();
+app.use(express.json())
 
-app.post('/', (req, res) => {
-  // Salvar no banco de dados por
+const sonicChannelIngest = new Ingest({
+  host: 'localhost',
+  port: 1491,
+  auth: 'SecretPassword',
+});
+
+const sonicChannelSearch = new Search({
+  host: 'localhost',
+  port: 1491,
+  auth: 'SecretPassword',
+});
+
+sonicChannelIngest.connect({
+  connected: () => {
+    console.log('Connected Ingest');
+  }
+});
+
+sonicChannelSearch.connect({
+  connected: () => {
+    console.log('Connected Search');
+  }
+});
+
+app.post('/pages', async (req, res) => {
+  const { title, content } = req.body;
+  const id = uuid();
+
+  // Cadastrar pagina no banco
+
+  await sonicChannelIngest.push('pages', 'default', `page:${id}`, `${title} ${content}`, {
+    lang: 'por'
+  })
 
   return res.status(201).send();
 });
 
-app.get('/search', (req, res) => {
-  // buscar registros
+app.get('/search', async (req, res) => {
 
-  return res.json([]);
+  const { query } = req.query;
+
+  const results = await sonicChannelSearch.query('pages', 'default', query, { lang: 'por' });
+  
+  return res.json(results);
 });
 
-app.get('/suggest', (req, res) => {
-  // buscar registros
+app.get('/suggest', async (req, res) => {
 
-  return res.json([]);
+  const { query } = req.query;
+
+  const results = await sonicChannelSearch.suggest('pages', 'default', query, { limit: 5 }, { lang: 'por' });
+  
+  return res.json(results);
 });
 
 app.listen(3333, () => {
